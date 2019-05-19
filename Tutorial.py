06492ee81388de81387c7776c38dff4aad3cc108
Jacob Cappi <jacob.cappi@protonmail.com>
2019-05-18 21:47:11 -0500

    Edits from tutorial: teaches main loop stuff.

--- Tutorial.py
+++ Tutorial.py
@@ -6,7 +6,31 @@ from pygame.locals import *
 
 # 2 - Initialize the game
 pygame.init()
-width, height = 640, 480
-screen=pygame.display.set_mode((width, height))
+width, height = 600, 800
+screen = pygame.display.set_mode((width, height))
 
-pygame.init()
+# Names the window
+pygame.display.set_caption('Hello')
+
+# the fps (in game time)
+clock = pygame.time.Clock()
+
+
+# Game loop (the main loop)
+
+crashed = False
+
+while not crashed:
+# Basic events (mouse, key press / frame)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            crashed = True
+
+        print(event)
+# This is the event in foreground
+    pygame.display.update()
+# Fps
+    clock.tick(30)
+
+pygame.quit()
+quit()
